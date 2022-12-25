f52b5e5b78d45fb4f4ddc1d6317c0fca84b199fc
Subhadiptech <subhadipdhn@gmail.com>
2022-12-25 16:59:39 +0530

    basic functionalities added

---
+++ MultiBottomSheets/src/main/java/com/ersubhadip/multibottomsheets/MultiBottomSheet.kt
@@ -0,0 +1,48 @@
+package com.ersubhadip.multibottomsheets
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+
+class MultiBottomSheet {
+    lateinit var bottomSheetDialog: BottomSheetDialog
+    lateinit var sheetView: View
+
+    constructor(context: Context?, view: View?, isCancellable: Boolean = false) {
+        sheetView = view!!
+        bottomSheetDialog = BottomSheetDialog(context!!)
+        bottomSheetDialog.apply {
+            setContentView(sheetView)
+            setCancelable(isCancellable)
+        }
+    }
+
+    constructor(context: Context?, view: View?) {
+        sheetView = view!!
+        bottomSheetDialog = BottomSheetDialog(context!!)
+        bottomSheetDialog.apply {
+            setContentView(sheetView)
+            setCancelable(false)
+        }
+    }
+
+    fun displayType(isShow: Boolean) {
+        if (isShow) {
+            bottomSheetDialog.show()
+        } else {
+            if (bottomSheetDialog.isShowing) {
+                bottomSheetDialog.dismiss()
+            }
+        }
+    }
+
+//    open fun sheetOnCancelledListener() {
+//        bottomSheetDialog.setOnCancelListener {
+//
+//        }
+//    }
+
+}
+
---
+++ MultiBottomSheets/src/main/java/com/ersubhadip/multibottomsheets/SheetListener.kt
@@ -0,0 +1,5 @@
+package com.ersubhadip.multibottomsheets
+
+interface SheetListener {
+
+}
